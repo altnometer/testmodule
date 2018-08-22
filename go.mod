93ce9939d0f446f932a046d419e5a6d011e1607f
sam <sam@redmoo.info>
2018-08-22 18:25:16 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/altnometer/testmodule
---
+++ testmodule.go
@@ -0,0 +1,8 @@
+package testmodule
+
+import "fmt"
+
+// Hi returns a friendly greeting.
+func Hi(name string) string {
+	return fmt.Sprintf("Hello, %s.", name)
+}
